1b4080e6df79b858e77ffe135c1f35666b20c7e9
Exokem <ex010@live.com>
2021-02-26 18:26:06 -0500

    Added StyledButton class to simplify styling of buttons

---
+++ src/main/java/xkv/visual/controls/StyledButton.java
@@ -0,0 +1,12 @@
+package xkv.visual.controls;
+
+import javafx.scene.control.Button;
+import xkv.visual.css.IStylable;
+
+public class StyledButton extends Button implements IStylable
+{
+    public StyledButton(String text)
+    {
+        super(text);
+    }
+}
